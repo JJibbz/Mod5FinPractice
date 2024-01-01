b4fc0c859eb1d43b467bc74319488439cf2e8e22
JJibbz <vkazancev322@gmail.com>
2024-01-01 19:56:08 +0700

    Ошибка CS0165

--- Mod5FinPractice/Program.cs
+++ Mod5FinPractice/Program.cs
@@ -1,12 +1,15 @@
-﻿namespace Mod5FinPractice
+﻿using System.Drawing;
+
+namespace Mod5FinPractice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
+            var x = Form();
+            ShowResult(x);
         }
-        static string Form(string name, string lastName, int age, bool pet, int petQuant, string[] petName, string[] favColors)
+        static (string name, string lastName, int age, bool pet, int petQuant, string[] petName, string[] favColors) Form()
         {
             (string name, string lastName, int age, bool pet, int petQuant, string[] petName, string[] favColors) anketa;
             Console.WriteLine("Введите ваше имя:");
@@ -17,12 +20,7 @@
             anketa.age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите количество любимых цветов цифрами:");
             int colorQuant = Convert.ToInt32(Console.ReadLine());
-            anketa.favColors = new string[colorQuant];
-            for (int i = 0; i <= favColors.Length; i++)
-            {
-                Console.WriteLine("Введите любимый цвет:");
-                anketa.favColors[i] = Console.ReadLine();
-            }
+            anketa.favColors = Colors(colorQuant);
             Console.WriteLine("Есть ли у вас питомцы? Если есть введите 1, иначе введите 0");
             int a = Convert.ToInt32(Console.ReadLine());
             if (a == 1)
@@ -32,12 +30,8 @@
                 {
                     Console.WriteLine("Введите количество ваших питомцев цифрами:");
                     anketa.petQuant = Convert.ToInt32(Console.ReadLine());
-                    string[] petname = new string[anketa.petQuant];
-                    for (int i = 0; i <= petname.Length; i++)
-                    {
-                        Console.WriteLine("Введите имя питомца:");
-                        petname[i] = Console.ReadLine();
-                    }
+                    anketa.petName = PetArray(anketa.petQuant);
+               
                 }
             }
             else
@@ -46,11 +40,52 @@
                     anketa.pet = false;
                 else
                     for (int i = a; i != 0 && i != 1; a = Convert.ToInt32(Console.ReadLine()))
+                    {
                         Console.WriteLine("Некорректный ввод, введите 1 или 0 в зависимости от наличия питомца.");
-
+                        Console.WriteLine("Есть ли у вас питомцы? Если есть введите 1, иначе введите 0");
+                    }
 
             }
+            return anketa; 
         }
-        static string[] PetArray(int num); { }
+        static string[] PetArray(int num) 
+        {
+            string[] petname = new string[num];
+            for (int i = 0; i <= petname.Length; i++)
+            {
+                Console.WriteLine("Введите имя питомца:");
+                petname[i] = Console.ReadLine();
+            }
+            return petname;
+        }
+        static string[] Colors(int num) 
+        {
+            string[] favColors = new string[num];
+            for (int i = 0; i <= favColors.Length; i++)
+            {
+                Console.WriteLine("Введите любимый цвет:");
+                favColors[i] = Console.ReadLine();
+            }
+            return favColors;
+        }
+        static void ShowResult((string name, string lastName, int age, bool pet, int petQuant, string[] petName, string[] favColors) person) 
+        {
+            Console.WriteLine($"Ваше имя: {person.name}. \nВаша фамилия: {person.lastName}. \nВаш возраст: {person.age}.");
+            if (person.pet == true)
+            {
+                Console.WriteLine($"У вас есть {person.petQuant} питомц(а/ев)!");
+                Console.WriteLine("Клички ваш(его/их) питомц(а/ев)");
+                foreach (string item in person.petName)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (string item in person.favColors)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
     }
 }
